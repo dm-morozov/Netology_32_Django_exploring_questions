795ed4a46c1aa0bd8ee37c0c11f74c4972e717ee
Dmitriy Morozov (PC) <dem.morozov@gmail.com>
2024-09-24 16:07:28 +0500

    add models Tag and Article

--- app_exploring_questions/models.py
+++ app_exploring_questions/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    # id
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=10)
+
+
+class Article(models.Model):
+    # id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='articles')
+    tags = models.ManyToManyField(Tag, related_name='articles')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
